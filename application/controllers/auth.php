d155152525d390225aee8ef52e0bab1430b92af1
tama <codenta@users.noreply.github.com>
2020-04-07 13:25:00 +0700

    update

---
+++ application/controllers/auth.php
@@ -0,0 +1,22 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class auth extends CI_Controller
+{
+
+    public function index()
+    {
+
+        $this->load->view('auth/auth_header');
+        $this->load->view('auth/login');
+        $this->load->view('auth/auth_footer');
+    }
+
+    public function register()
+    {
+
+        $this->load->view('auth/auth_header');
+        $this->load->view('auth/register');
+        $this->load->view('auth/auth_footer');
+    }
+};
